9764df708e9ae10e457048b80d2c260956863332
Viktoras Domarkas <viktoras@bravelittletank.com>
2024-09-24 17:17:04 +0100

    add tailwindcss to config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,2 +1,9 @@
-let mix = require('laravel-mix');
-mix.js('src/js/app.js', 'assets').sass('src/scss/app.scss', 'assets');
+let mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
+
+mix.js("src/js/app.js", "assets")
+	.sass("src/scss/app.scss", "assets")
+	.options({
+		processCssUrls: false,
+		postCss: [tailwindcss("tailwind.config.js")],
+	});
